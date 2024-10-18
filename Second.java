481a2b97b8d3ffe059629f56a3ce076ea0b6533c
Ubai Ratangiri <112539947+ubaicoder@users.noreply.github.com>
2024-10-18 16:38:14 +0530

    Update Second.java

--- Second.java
+++ Second.java
@@ -1,16 +1,8 @@
 import java.util.Scanner; // Import the Scanner class
 
-class Second {
-  public static void main(String[] args) {
-    int x, y, sum;
-    Scanner myObj = new Scanner(System.in); // Create a Scanner object
-    System.out.println("Type a number:");
-    x = myObj.nextInt(); // Read user input
-
-    System.out.println("Type another number:");
-    y = myObj.nextInt(); // Read user input
-
-    sum = x + y;  // Calculate the sum of x + y
-    System.out.println("Sum is: " + sum); // Print the sum
-  }
-} 
+public class Second {
+    public static void main(String[] args) {
+        int number = 5;  // Hardcoded value
+        System.out.println("You entered: " + number);
+    }
+}
